63556ea16daca52287d8f5bf64d9bf7829141265
mearvinddevda <arvinddevda02@gmail.com>
2024-12-10 17:32:10 +0530

    commiting

--- index.js
+++ index.js
@@ -9,26 +9,26 @@ import subscriberRouter from "./router/subscriber.route.js"
 import contactFormRouter from "./router/contactform.route.js"
 const app = express();
 dotenv.config({});
-// const corsOptions = {
-//   origin: 'https://real-trust-front-end-y9p9.vercel.app',  
+const corsOptions = {
+  origin: 'https://real-trust-front-end-y9p9.vercel.app',  
   
-//   // origin: 'http://localhost:5173',
+  // origin: 'http://localhost:5173',
 
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
-// };
-// app.use(cors(corsOptions));
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+};
+app.use(cors(corsOptions));
 
-// app.options('*', (req, res) => {
-//   // res.header('Access-Control-Allow-Origin', 'https://real-trust-front-end-y9p9.vercel.app');  
-//   res.header('Access-Control-Allow-Origin', 'https://real-trust-front-end-y9p9.vercel.app');
+app.options('*', (req, res) => {
+  // res.header('Access-Control-Allow-Origin', 'https://real-trust-front-end-y9p9.vercel.app');  
+  res.header('Access-Control-Allow-Origin', 'https://real-trust-front-end-y9p9.vercel.app');
 
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.status(200).end();
-// }); 
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.status(200).end();
+}); 
 
 app.get("/",(req,res)=>{
     res.status(200).send("hello")
