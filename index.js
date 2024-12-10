13972c5ec0c6d81797af6e8a81d2460e6b76da22
mearvinddevda <arvinddevda02@gmail.com>
2024-12-10 17:00:25 +0530

    Something new

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cors(corsOptions));
 
 app.options('*', (req, res) => {
   // res.header('Access-Control-Allow-Origin', 'https://real-trust-front-end-y9p9.vercel.app');  
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://real-trust-front-end-y9p9.vercel.app');
 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
